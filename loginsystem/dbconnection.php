ccc8d639f16ab4ce317a3f43e557eec4c1fd755b
siteshbade <49814332+siteshbade@users.noreply.github.com>
2021-06-01 17:37:20 +0530

    Update dbconnection.php

--- loginsystem/dbconnection.php
+++ loginsystem/dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
 define('DB_USER','root');
-define('DB_PASS' ,'root1234'); //update password
+define('DB_PASS' ,'root1234'); //update password of root user
 define('DB_NAME', 'loginsystem');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
